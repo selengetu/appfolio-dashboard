94ce80c15d936643c90b778c7ec9e1d7a55a49d4
Selenge Tulga <49024084+selengetu@users.noreply.github.com>
2025-03-18 10:26:26 -0500

    mm

--- streamlit.py
+++ streamlit.py
@@ -56,9 +56,9 @@ with tab1:
     # Calculate occupancy percentage
     occupied = (current_units / all_units) * 100
     
-    dfs["Tenant Data"]["Rent"] = dfs["Tenant Data"]["Rent"].replace("[\$,]", "", regex=True)  # Remove $ and ,
+    dfs["Tenant Data"]["Rent"] = dfs["Tenant Data"]["Rent"].replace("[\,]", "", regex=True)  # Remove $ and ,
     dfs["Tenant Data"]["Rent"] = pd.to_numeric(dfs["Tenant Data"]["Rent"], errors="coerce")  # Convert to number
-    dfs["Tenant Data"]["Market Rent"] = dfs["Tenant Data"]["Market Rent"].replace("[\$,]", "", regex=True)  # Remove $ and ,
+    dfs["Tenant Data"]["Market Rent"] = dfs["Tenant Data"]["Market Rent"].replace("[\,]", "", regex=True)  # Remove $ and ,
     dfs["Tenant Data"]["Market Rent"] = pd.to_numeric(dfs["Tenant Data"]["Market Rent"], errors="coerce")  # Convert to number
 
     # Calculate total rent
